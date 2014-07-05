5c22f36f3999419d66fe5913f1b363f9aa0ed5e9
jden <jason@denizac.org>
2014-07-05 10:45:58 -0700

    impl

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # rel-send
-handle routing and sending a rel message
+handle routing and sending a [rel](http://rel.is) message
 
 ## usage
 ```js
 var relSend = require('rel-send')
+
+// a rel message
+var message = {
+  "@context":"http://rel.is/0.1",
+  "to":"http://foo.com",
+  "from":"http://bar.org",
+  "rel":"http://gmpg.org/xfn/11/friend"
+}
+
+relsend(message, function (e) {
+  if (e) { return console.log('could not send')}
+})
 ```
-
-
-## api
 
 
 ## installation
--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-function relSend () {
-  
+var request = require('request')
+var webfinger = require('webfinger').webfinger
+
+var FALLBACK = module.exports.FALLBACK = 'http://alpha.rel.is'
+
+function send(message, cb) {
+  getDestinationServer(message.to, function (e, server) {
+    console.log('dest server:', server)
+    if (e) { return cb(e) }
+    request({
+      uri: server + '/messages',
+      method: 'post',
+      json: message
+    }, function (e, res) {
+      if (e) { return cb(e) }
+      if (res.statusCode >= 400) {
+        return cb(new Error(res.statusCode + ' ' + res.body))
+      }
+      cb()
+    })
+  })
 }
 
-module.exports = relSend
+function getDestinationServer(recipient, cb) {
+  // console.trace(recipient)
+  getRelEx(recipient, function (e, link) {
+    console.trace(link)
+    if (e) { return cb(e) }
+    if (!link || !link.href) {
+      return cb(null, FALLBACK)
+    }
+    cb(null, link.href)
+  })
+}
+
+function getRelEx(address, cb) {
+  // todo: depend on indiefinger code directly to make more decentralized
+  request({
+    uri: 'http://indiefinger.org/.well-known/webfinger?resource='+encodeURIComponent(address),
+    json: true
+  }, function (e, res, jrd) {
+    if (e) { return cb(e) }
+    var rel = jrd.links.filter(function (link) {
+      return link.rel === 'http://rel.is/0.1'
+    })[0]
+  console.log(rel)
+    cb(null, rel)
+  })
+}
+
+module.exports = send
